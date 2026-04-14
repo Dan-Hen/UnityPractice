a9e08fce3597df2b17d900c65a2a6ba178c3d446
Dan Hen <leo.clot16@gmail.com>
2026-04-13 20:59:09 -0400

    Add mesh and mecha

---
+++ Assets/Script/GameFlow.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class GameFlow : MonoBehaviour
+{
+    /* General Flow
+
+    ----- start of the game ----- 
+
+    enter trigger box
+    turn on input e for my game and switch off any unwanted behavior from other games 
+    press "e" to play
+    sweep player available in the world 
+    choose randomly a player 
+    create role hunter and hunted
+    notify the hunter
+    notify the hunted
+    start timer
+
+    ---- Win / Found ----
+    notify both player
+    add time + bonus to scoreboard 
+
+    ---- lose / not found ----
+    notify both player
+    add time + malus to scoreboard
+
+
+    ----- at any time -----
+
+    press "f" to give up
+
+    
+    */
+
+    void Start()
+    {
+        
+    }
+
+    void playerSweep()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
